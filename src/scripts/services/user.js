6aaf5d00266b394b847696962807bc543a0615a0
Paulo Prata <pauloprata10@gmail.com>
2022-08-18 09:33:24 -0300

    adicionando recursos

--- src/scripts/services/user.js
+++
@@ -1,8 +0,0 @@
-import { baseUrl } from '/src/scripts/variables.js'
-
-async function getUser(userName) {
-  const response = await fetch(`${baseUrl}/${userName}`)
-  return await response.json()
-}
-
-export { getUser }
