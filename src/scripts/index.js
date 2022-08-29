ec36e00c65dc4060e82578ef3997f6cd90f6f779
Paulo Prata <pauloprata10@gmail.com>
2022-08-29 20:47:44 -0300

    projeto finalizado

---
+++ src/scripts/index.js
@@ -0,0 +1,49 @@
+import { getUser } from '../scripts/services/user.js'
+import { getRepositories } from '../scripts/services/repositories.js'
+
+import { user } from '../scripts/objects/user.js'
+import { screen } from '../scripts/objects/screen.js'
+
+document.getElementById("btn-search").addEventListener("click", () => {
+  const userName = document.getElementById("input-search").value;
+  if (validateEmptyInput(userName)) return;
+  getUserData(userName);
+});
+
+document.getElementById("input-search").addEventListener("keyup", (e) => {
+  const userName = e.target.value;
+  const key = e.which || e.keyCode;
+  const isEnterKeyPressed = key === 13;
+
+  if (isEnterKeyPressed) {
+    if (validateEmptyInput(userName)) return;
+    getUserData(userName);
+  }
+});
+
+function validateEmptyInput(userName) {
+  if (userName.length === 0) {
+    alert("Preencha o campo com o nome do usuário do GitHub");
+    return true;
+  }
+}
+
+async function getUserData(userName) {
+
+  const userResponse = await getUser(userName)
+
+  console.log(userResponse)
+  
+  if(userResponse.message === "Not Found") {
+    screen.renderNotFound()
+    return
+  }
+  
+  
+  const repositoriesResponse = await getRepositories(userName);
+  
+  user.setInfo(userResponse);
+  user.setRepositories(repositoriesResponse);
+  
+  screen.renderUser(user);
+}
